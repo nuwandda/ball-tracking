ed4c568b83bd56f73d15345962de2359485ef866
Bugrahan <donmezbugrahan@gmail.com>
2019-05-06 22:43:03 +0300

    Add Person Detector system

---
+++ ball-tracking-backboard/person_detection.py
@@ -0,0 +1,120 @@
+# import the necessary packages
+import numpy as np
+import time
+import cv2
+import os
+
+
+class PersonDetection:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def detect(src_image, boundary_x, boundary_y, boundary_w, boundary_h):
+        path = '/Users/nuwanda/Documents/GitHub/development/ball-tracking/ball-tracking-backboard/yolo-coco'
+        src_shot_location = np.array([])
+        # minimum probability to filter weak detections
+        default_confidence = 0.5
+        # threshold when applying non-maxima suppression
+        default_threshold = 0.3
+
+        # load the COCO class labels our YOLO model was trained on
+        labelsPath = os.path.sep.join([path, "coco.names"])
+        LABELS = open(labelsPath).read().strip().split("\n")
+
+        # initialize a list of colors to represent each possible class label
+        np.random.seed(42)
+        COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+                                   dtype="uint8")
+
+        # derive the paths to the YOLO weights and model configuration
+        weightsPath = os.path.sep.join([path, "yolov3.weights"])
+        configPath = os.path.sep.join([path, "yolov3.cfg"])
+
+        # load our YOLO object detector trained on COCO dataset (80 classes)
+        print("[INFO] loading YOLO from disk...")
+        net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
+        # load our input image and grab its spatial dimensions
+        image = src_image
+        (H, W) = image.shape[:2]
+
+        # determine only the *output* layer names that we need from YOLO
+        ln = net.getLayerNames()
+        ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+        # construct a blob from the input image and then perform a forward
+        # pass of the YOLO object detector, giving us our bounding boxes and
+        # associated probabilities
+        blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
+                                     swapRB=True, crop=False)
+        net.setInput(blob)
+        start = time.time()
+        layerOutputs = net.forward(ln)
+        end = time.time()
+
+        # show timing information on YOLO
+        print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+
+        # initialize our lists of detected bounding boxes, confidences, and
+        # class IDs, respectively
+        boxes = []
+        confidences = []
+        classIDs = []
+
+        # loop over each of the layer outputs
+        for output in layerOutputs:
+            # loop over each of the detections
+            for detection in output:
+                # extract the class ID and confidence (i.e., probability) of
+                # the current object detection
+                scores = detection[5:]
+                classID = np.argmax(scores)
+                confidence = scores[classID]
+
+                # filter out weak predictions by ensuring the detected
+                # probability is greater than the minimum probability
+                if confidence > default_confidence:
+                    # scale the bounding box coordinates back relative to the
+                    # size of the image, keeping in mind that YOLO actually
+                    # returns the center (x, y)-coordinates of the bounding
+                    # box followed by the boxes' width and height
+                    box = detection[0:4] * np.array([W, H, W, H])
+                    (centerX, centerY, width, height) = box.astype("int")
+
+                    # use the center (x, y)-coordinates to derive the top and
+                    # and left corner of the bounding box
+                    x = int(centerX - (width / 2))
+                    y = int(centerY - (height / 2))
+
+                    # update our list of bounding box coordinates, confidences,
+                    # and class IDs
+                    boxes.append([x, y, int(width), int(height)])
+                    confidences.append(float(confidence))
+                    classIDs.append(classID)
+
+        # apply non-maxima suppression to suppress weak, overlapping bounding
+        # boxes
+        idxs = cv2.dnn.NMSBoxes(boxes, confidences, default_confidence,
+                                default_threshold)
+
+        # ensure at least one detection exists
+        if len(idxs) > 0:
+            # loop over the indexes we are keeping
+            for i in idxs.flatten():
+                # extract the bounding box coordinates
+                (x, y) = (boxes[i][0], boxes[i][1])
+                (w, h) = (boxes[i][2], boxes[i][3])
+
+                if int(boundary_x * 0.8) < x < int(boundary_x + boundary_w * 2) and int(boundary_y * 0.8) < y < int(
+                        boundary_y + boundary_h * 2):
+                    # draw a bounding box rectangle and label on the image
+                    color = [int(c) for c in COLORS[classIDs[i]]]
+                    cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+                    text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+                    cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+                                0.5, color, 2)
+
+                    src_shot_location = np.append(src_shot_location, [x + w / 2, y + h])
+
+        return src_shot_location
