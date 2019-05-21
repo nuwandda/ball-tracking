bf3253010183cc09e97ea66f9068c5313951a80c
Bugrahan <donmezbugrahan@gmail.com>
2019-05-21 23:23:58 +0300

    Add About window

--- ball-tracking-backboard/interface.py
+++ ball-tracking-backboard/interface.py
@@ -27,11 +27,12 @@ class Interface:
             event, values = window.ReadNonBlocking()
 
             if event == 'Load':
-                process = Process(values['path'])
+                play_border = sg.PopupGetText('Please type the number of play that will be processed.', 'Number of Plays')
+                process = Process(values['path'], play_border)
             elif event == 'Start':
                 sg.Popup(
                     'Please select with the order below:\nTop Left, Bottom Left, Bottom Right and Top Right.\nHit "e" for selection, "r" for select again.')
-                process.getInitFourPoints
+                process.getInitFourPoints()
                 sg.Popup('Please select the backboard.\nHit "Enter" for selection, "c" for select again.')
                 process.getInitROI()
                 process.start()
@@ -43,14 +44,22 @@ class Interface:
             elif event == 'About' and not about_window_active:
                 about_window_active = True
                 window.Hide()
-                layout_about = [[sg.Text('About')], [sg.Button('Exit')]]
-                about_window = sg.Window('About', layout_about)
+                layout_about = [[sg.Text('My Basketball Coach', size=(30, 1), font=("Helvetica", 25))],
+                                [sg.Text('To use the application, please choose a video from file browser.', size=(60, 1), font=("Helvetica", 15))],
+                                [sg.Text('Then, click "Load" button to create a new session.', size=(60, 1), font=("Helvetica", 15))],
+                                [sg.Text('Click "Start" to run the application.', size=(60, 1), font=("Helvetica", 15))],
+                                [sg.Text('Given number of plays will be processed after start.', size=(60, 1), font=("Helvetica", 15))],
+                                [sg.Text('After the heat map is created, click "Save" to save heat map as PNG.', size=(60, 1), font=("Helvetica", 15))],
+                                [sg.Text('Restart process to create another heat map or click "Quit" to exit the application.', size=(60, 1), font=("Helvetica", 15))],
+                                [sg.Text('Never say never because limits, like fears, are often just an illusion. -Michael Jordan', text_color="red" ,size=(80, 1), font=("Helvetica", 15))],
+                                [sg.Button('Exit')]]
+                about_window = sg.Window('About', default_element_size=(120, 1)).Layout(layout_about)
                 while True:  
                     event2, values2 = about_window.Read()  
                     if event2 == 'Exit':  
                         about_window.Close()  
                         about_window_active = False  
-                        window.UnHide()  
+                        window.UnHide()
                         break  
 
             if process is not None:
